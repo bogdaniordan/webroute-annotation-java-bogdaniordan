16df3f2de44be946ffcd55a0fa49224bc8b7f477
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-07-27 18:03:11 +0300

    Added optimal solution

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,12 @@
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -12,8 +15,20 @@ import controller.Routes;
 import controller.WebRoute;
 
 public class Test {
+    private static final HashMap<String, Method> pathToMethods = new HashMap<>();
+    private static final Routes ROUTES = new Routes();
+
+    private static void storeMethodsAndPaths() {
+        for (Method method: Routes.class.getMethods()) {
+            if (method.isAnnotationPresent(WebRoute.class)) {
+                String path = method.getAnnotation(WebRoute.class).path();
+                pathToMethods.put(path, method);
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
+        storeMethodsAndPaths();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/home", new MyHandler());
         server.createContext("/sign-in", new MyHandler());
@@ -21,21 +36,23 @@ public class Test {
         server.start();
     }
 
+
+
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            for (Method method: Routes.class.getMethods()) {
-                if (method.isAnnotationPresent(WebRoute.class)) {
-                    String path = method.getAnnotation(WebRoute.class).path();
-                    System.out.println(path);
-                    if (t.getRequestURI().toString().equals(path)) {
-                        String response = method.toGenericString();
-                        t.sendResponseHeaders(200, response.getBytes().length);
-                        OutputStream os = t.getResponseBody();
-                        os.write(response.getBytes());
-                        os.close();
-                    }
+            String path = t.getRequestURI().toString();
+            if (pathToMethods.containsKey(path)) {
+                String response = null;
+                try {
+                    response = (String) pathToMethods.get(path).invoke(ROUTES);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
                 }
+                t.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
             }
         }
     }
